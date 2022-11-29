62c30a43ff8d56293b9c9b290667f201a37038a5
Pedro Dias <pedromiguelprdias@gmail.com>
2022-11-29 20:52:49 +0000

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
 const app = () => {
   return (
-    <div>app</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
