a66d146d20249517a521e7324d2f7d3ca0efe1ad
Pedro Dias <pedromiguelprdias@gmail.com>
2022-11-29 22:48:35 +0000

    Header first stage

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I am</h5>
+        <h1>Pedro Dias</h1>
+        <h5 className="text-light">Application Security Engineer</h5>
+        <CTA />
+        <HeaderSocial />
+        <a href="#contact" classname='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
